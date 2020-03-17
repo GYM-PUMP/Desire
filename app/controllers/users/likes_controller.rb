be51199c7efae2ced0f5c26496918c7216d08e52
GYM-PUMP <nnrda.nnrda@gmail.com>
2020-03-17 11:33:55 +0000

    いいね機能（非同期）実装

---
+++ app/controllers/users/likes_controller.rb
@@ -0,0 +1,15 @@
+class Users::LikesController < ApplicationController
+  def create
+        @supplement = Supplement.find(params[:supplement_id])
+        like = current_user.likes.new(supplement_id: @supplement.id)
+        like.save
+        # redirect_back(fallback_location: root_path)非同期の場合は不要
+    end
+
+    def destroy
+        @supplement = Supplement.find(params[:supplement_id])
+        like = current_user.likes.find_by(supplement_id: @supplement.id)
+        like.destroy
+        # redirect_back(fallback_location: root_path)非同期の場合は不要
+    end
+end
