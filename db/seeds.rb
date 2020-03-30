63b9f9e466b400903815e085cf5c5026c01d6a2a
GYM-PUMP <nnrda.nnrda@gmail.com>
2020-03-30 07:59:32 +0000

    admin sign up & admin create(seeds)

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Admin.create(id:2, email: 'admin@admin', password: adminadmin)
