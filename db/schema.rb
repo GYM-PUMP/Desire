fdbfc8ea2bd9873dfefb3531957cbb48fef888c5
GYM-PUMP <nnrda.nnrda@gmail.com>
2020-04-15 06:50:05 +0000

    remove rate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_04_121459) do
+ActiveRecord::Schema.define(version: 2020_04_15_064452) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -37,7 +37,6 @@ ActiveRecord::Schema.define(version: 2020_04_04_121459) do
     t.integer "user_id"
     t.integer "supplement_id"
     t.text "body"
-    t.float "rate"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.decimal "score", precision: 5, scale: 3
