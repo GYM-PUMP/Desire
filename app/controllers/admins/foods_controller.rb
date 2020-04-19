9d07ea3a01874167ea938a95e94b0b855c886ac7
GYM-PUMP <nnrda.nnrda@gmail.com>
2020-04-19 14:08:25 +0000

    create admin/food(view)

--- app/controllers/admins/foods_controller.rb
+++ app/controllers/admins/foods_controller.rb
@@ -1,2 +1,57 @@
 class Admins::FoodsController < ApplicationController
+	before_action :authenticate_admin!
+
+	def index
+		@foods = Food.page(params[:page]).reverse_order.per(10)
+		@food_genres = FoodGenre.all
+		if params[:category].present?
+			# もしcategoryにデータが入っている場合
+			@food_genre = @food_genres.find_by(food_genre_name: params[:category])
+			# ビューで渡した:categoryでgenre_nameを一致させそれを@genreとしてる
+			@foods = Food.where(food_genre_id: @food_genre.id)
+			 foods = Food.where(food_genre_id: @food_genre.id)
+			@foods_quantity = foods.length
+		else
+
+		end
+	end
+
+	def create
+		@food = Food.new(food_params)
+		@food_genres = FoodGenre.all
+		if @food.save
+			redirect_to admins_food_path(@food)
+		else
+			render 'new'
+		end
+	end
+
+	def new
+		@food = Food.new
+		@food_genres = FoodGenre.all
+	end
+
+	def edit
+		@food = Food.find(params[:id])
+	end
+
+	def show
+		@food = Food.find(params[:id])
+	end
+
+
+	def update
+		@food = Food.find(params[:id])
+		if @food.update(food_params)
+			redirect_to admins_food_path
+		else
+			render "edit"
+		end
+	end
+
+
+    private
+	def food_params
+		params.require(:food).permit(:food_genre_id, :food_name, :food_content, :food_image, :ingestion_cal, :protein, :fat, :carb)
+	end
 end
