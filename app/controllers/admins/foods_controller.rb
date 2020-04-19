44aba63dc88432c11bf20aedd860e96e2025529d
GYM-PUMP <nnrda.nnrda@gmail.com>
2020-04-19 17:52:02 +0000

    create training(view)

--- app/controllers/admins/foods_controller.rb
+++ app/controllers/admins/foods_controller.rb
@@ -3,17 +3,6 @@ class Admins::FoodsController < ApplicationController
 
 	def index
 		@foods = Food.page(params[:page]).reverse_order.per(10)
-		@food_genres = FoodGenre.all
-		if params[:category].present?
-			# もしcategoryにデータが入っている場合
-			@food_genre = @food_genres.find_by(food_genre_name: params[:category])
-			# ビューで渡した:categoryでgenre_nameを一致させそれを@genreとしてる
-			@foods = Food.where(food_genre_id: @food_genre.id)
-			 foods = Food.where(food_genre_id: @food_genre.id)
-			@foods_quantity = foods.length
-		else
-
-		end
 	end
 
 	def create
--- app/controllers/admins/trainings_controller.rb
+++ app/controllers/admins/trainings_controller.rb
@@ -1,2 +1,57 @@
 class Admins::TrainingsController < ApplicationController
+	before_action :authenticate_admin!
+
+	def index
+		@trainings = Training.page(params[:page]).reverse_order.per(10)
+		@training_genres = TrainingGenre.all
+		if params[:category].present?
+			# もしcategoryにデータが入っている場合
+			@training_genre = @training_genres.find_by(training_genre_name: params[:category])
+			# ビューで渡した:categoryでgenre_nameを一致させそれを@genreとしてる
+			@trainings = Training.where(training_genre_id: @training_genre.id)
+			 trainings = Training.where(training_genre_id: @training_genre.id)
+			@trainings_quantity = trainings.length
+		else
+
+		end
+	end
+
+	def create
+		@training = Training.new(training_params)
+		@training_genres = Training.all
+		if @training.save
+			redirect_to admins_training_path(@training)
+		else
+			render 'new'
+		end
+	end
+
+	def new
+		@training = Training.new
+		@training_genres = Training.all
+	end
+
+	def edit
+		@training = Training.find(params[:id])
+	end
+
+	def show
+		@training = Training.find(params[:id])
+	end
+
+
+	def update
+		@training = Training.find(params[:id])
+		if @training.update(training_params)
+			redirect_to admins_training_path
+		else
+			render "edit"
+		end
+	end
+
+
+    private
+	def training_params
+		params.require(:training).permit(:training_genre_id, :training_name, :training_content, :training_image, :movie_url, :consumed_cal)
+	end
 end
