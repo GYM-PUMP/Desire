439840eb6a39f008f9608560216d5b5c1449557e
GYM-PUMP <nnrda.nnrda@gmail.com>
2020-04-25 14:25:52 +0000

    [edit] all_layout

--- app/models/my_menu.rb
+++ app/models/my_menu.rb
@@ -2,4 +2,7 @@ class MyMenu < ApplicationRecord
 	belongs_to :user, optional: true
 	belongs_to :food, optional: true
 	belongs_to :training, optional: true
+
+	# validates :training_quantity, presence: true
+	# validates :food_quantity, presence: true
 end
