07c7c1a1a2950251fe355f27ab4e385112500547
GYM-PUMP <nnrda.nnrda@gmail.com>
2020-05-22 10:04:31 +0000

    [fixed] action mailer

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,7 +1,14 @@
 class ContactMailer < ApplicationMailer
-	def send_user(user) #メソッドに対して引数を設定
+  def send_user(user) #メソッドに対して引数を設定
     @user = User.find(user.user_id)
- 	# binding.pry
     mail(to: @user.email, subject: '【株式会社  BIG Muscle】 ご意見いただき誠にありがとうございます。')
   end
+
+  def send_mail(contact)
+  @contact = contact
+  mail(
+    to:   ENV["ADMIN_EMAIL"],
+    subject: 'お問い合わせ通知'
+  )
+end
 end
